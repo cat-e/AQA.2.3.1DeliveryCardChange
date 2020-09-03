aeb17c99194dd5cc9d60c855266600ab39782bd5
cat-e <khlebalova.e@yandex.ru>
2020-09-03 23:17:21 +0300

    fix test and added ci

--- src/test/java/ru/netology/test/DeliveryCardChangeTest.java
+++ src/test/java/ru/netology/test/DeliveryCardChangeTest.java
@@ -1,10 +1,13 @@
 package ru.netology.test;
 
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
@@ -12,25 +15,35 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class DeliveryCardChangeTest {
+    private Faker faker;
+    @BeforeEach
+    void setUpAll() {
+        faker = new Faker(new Locale("ru"));
+    }
+
+
     @Test
     void shouldSendDataForDelivery() {
         String date = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         String date1 = LocalDate.now().plusDays(20).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        String name = faker.name().fullName();
+        String phone = faker.phoneNumber().phoneNumber();
+        String city = faker.address().city();
         open("http://localhost:9999/");
-        $("[placeholder='Город']").setValue("Самара");
+        $("[placeholder='Город']").setValue(city);
         $(".calendar-input__custom-control input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $(".calendar-input__custom-control input").setValue(date);
-        $("[name='name']").sendKeys("Василий Кот");
-        $("[name='phone']").sendKeys("+79171249001");
+        $("[name='name']").setValue(name);
+        $("[name='phone']").setValue(phone);
         $("[data-test-id=agreement] .checkbox__box").click();
         $("button.button").click();
-        $("[data-test-id=success-notification]").shouldHave(text("Встреча успешно запланирована на " + date));
+        $("[data-test-id=success-notification]").waitUntil(visible, 15000).shouldHave(text("Встреча успешно запланирована на " + date));
         $(".calendar-input__custom-control input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $(".calendar-input__custom-control input").setValue(date1);
         $("button.button").click();
         $("[data-test-id=replan-notification]").shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
-        $("button").shouldHave(text("Перепланировать")).click();
-        $("[data-test-id=success-notification]").shouldHave(text("Встреча успешно запланирована на " + date1));
+        $("[data-test-id=replan-notification] button.button").click();
+        $("[data-test-id=success-notification]").waitUntil(visible, 15000).shouldHave(text("Встреча успешно запланирована на " + date1));
 
 
     }
