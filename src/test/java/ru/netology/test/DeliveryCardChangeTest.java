f8ff31685cdad0f002de34714ffaada694a361be
cat-e <khlebalova.e@yandex.ru>
2020-09-05 00:18:25 +0300

    divide data and test

--- src/test/java/ru/netology/test/DeliveryCardChangeTest.java
+++ src/test/java/ru/netology/test/DeliveryCardChangeTest.java
@@ -1,40 +1,30 @@
 package ru.netology.test;
 
-import com.github.javafaker.Faker;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.DataGenerator.Registration.*;
 
 public class DeliveryCardChangeTest {
-    private Faker faker;
-    @BeforeEach
-    void setUpAll() {
-        faker = new Faker(new Locale("ru"));
-    }
-
 
     @Test
     void shouldSendDataForDelivery() {
         String date = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         String date1 = LocalDate.now().plusDays(20).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        String name = faker.name().fullName();
-        String phone = faker.phoneNumber().phoneNumber();
-        String city = faker.address().city();
+
         open("http://localhost:9999/");
-        $("[placeholder='Город']").setValue(city);
+        $("[placeholder='Город']").setValue(cityForCard("ru"));
         $(".calendar-input__custom-control input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $(".calendar-input__custom-control input").setValue(date);
-        $("[name='name']").setValue(name);
-        $("[name='phone']").setValue(phone);
+        $("[name='name']").setValue(nameForCard("ru"));
+        $("[name='phone']").setValue(phoneForCard("ru"));
         $("[data-test-id=agreement] .checkbox__box").click();
         $("button.button").click();
         $("[data-test-id=success-notification]").waitUntil(visible, 15000).shouldHave(text("Встреча успешно запланирована на " + date));
